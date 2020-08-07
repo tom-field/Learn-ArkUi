a70559155f5bee09c9b53e245f778052f2ee6d45
许辉 <xu.h@burgeon.cn>
2020-08-07 11:36:37 +0800

    添加路由并引入arkui

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import {Routers} from './router.js';
+import ArkUi from '@syman/ark-ui'
+import '@syman/ark-ui/dist/styles/ark-ui.css'
+import '@syman/ark-ui/dist/styles/bjIconfonts/iconfont.css';
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+Vue.use(ArkUi);
+
+// The routing configuration
+const RouterConfig = {
+  routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router: router,
+  render: h => h(App)
+});
